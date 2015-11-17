81fea84b787db35741a0d45311f4e3dd0e71b697
Curtis Larson <quackware@gmail.com>
2015-11-17 10:22:51 -0500

    Add adsense code

---
+++ client/js/adsense.js
@@ -0,0 +1,4 @@
+Template.registerHelper("adsenseCode", function() {
+  var str = '<scriptasyncsrc="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!--RandomPowerHourResponsive--><insclass="adsbygoogle"style="display:block"data-ad-client="ca-pub-0183306563861637"data-ad-slot="7489052424"data-ad-format="auto"></ins><script>(adsbygoogle=window.adsbygoogle||[]).push({});</script>';
+  return str;
+});
